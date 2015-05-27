a64f2af4f5cc14a7ef01a586a9a38def670d9141
Martina8 <martina.casari.93@gmail.com>
2015-05-27 11:38:45 +0200

    prova

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 from attitude import *
 from preprocessing import *
-import nltk
-
+#prova
 text = "Magic is on the air"
 
 tokens = gavi_tokenize_no_stopwords(text)
 
 print tokens
 
-
 for t in tokens:
     print t
     t = gavi_stemming(t)
     print EVA(t)
     print ACT(t)
-    print POT(t)
-
